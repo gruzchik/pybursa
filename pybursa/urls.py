6ae1a56b6dda930c978751873e42db95b627fff2
gruzchik <ahtyrka@yandex.ru>
2014-11-26 04:19:36 +0200

    hometask 7

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -8,5 +8,8 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+    url(r'^index', TemplateView.as_view(template_name='index.html'), name='home'),
+    url(r'^list', TemplateView.as_view(template_name='list.html'), name='list'),
+    url(r'^first', TemplateView.as_view(template_name='first.html'), name='student'),
     url(r'^admin/', include(admin.site.urls)),
 )
