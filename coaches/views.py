dbcaad69073cd286b09ac3e958f3095804a3b169
gruzchik <ahtyrka@yandex.ru>
2014-12-22 00:36:43 +0200

    hw14 adv

--- coaches/views.py
+++ coaches/views.py
@@ -5,6 +5,15 @@ from coaches.models import Coach
 from django import forms
 
 
+class CoachForm(forms.Form):
+    COACH_TYPES = (('C', 'Coach'), ('A', 'Assistant'))
+    coach_name = forms.CharField(label='Name', max_length=100)
+    coach_lastname = forms.CharField(label='Lastname', max_length=255)
+    coach_email = forms.EmailField()
+    coach_phone = forms.CharField(max_length=13)
+    coach_ctype = forms.ChoiceField(choices=COACH_TYPES, widget=forms.RadioSelect)
+
+
 class CoachModelForm(forms.ModelForm):
 	class Meta:
 		model = Coach
@@ -21,17 +30,40 @@ def coach_info(request, coach_id):
     coach = get_object_or_404(Coach, id=coach_id)
     return render(request, 'coaches/coach_details.html', {'coach': coach})
 
+
 def coach_edit(request, coach_id):
-    title = "Coach edit item"
     coach = Coach.objects.get(id=coach_id)
     if request.method == 'POST':
-        form = CoachModelForm(request.POST, instance=coach)
+        form = CoachForm(request.POST)
         if form.is_valid():
-            coach = form.save()
-            return redirect('coaches_list')
+            coach.first_name = form.cleaned_data['coach_name']
+            coach.last_name = form.cleaned_data['coach_lastname']
+            coach.email = form.cleaned_data['coach_email']
+            coach.phone_number = form.cleaned_data['coach_phone']
+            coach.ctype = form.cleaned_data['coach_ctype']
+            coach.save()
+            return redirect('coach_edit', coach_id)
     else:
-        form = CoachModelForm(instance=coach)
-    return render(request, 'coaches/coach_edit.html', {'form': form, 'title': title})
+        form = CoachForm(initial={'coach_name':coach.first_name,
+        	                        'coach_lastname':coach.last_name,
+                                    'coach_email':coach.email,
+                                    'coach_phone':coach.phone_number,
+                                    'coach_ctype':coach.ctype,
+                                    })
+    return render(request, 'coaches/coach_edit.html', {'form': form})
+
+
+#def coach_edit(request, coach_id):
+#    title = "Coach edit item"
+#    coach = Coach.objects.get(id=coach_id)
+#    if request.method == 'POST':
+#        form = CoachModelForm(request.POST, instance=coach)
+#        if form.is_valid():
+#            coach = form.save()
+#            return redirect('coaches_list')
+#    else:
+#        form = CoachModelForm(instance=coach)
+#    return render(request, 'coaches/coach_edit.html', {'form': form, 'title': title})
 
 
 def coach_add(request):
@@ -39,7 +71,7 @@ def coach_add(request):
     if request.method == 'POST':
         form = CoachModelForm(request.POST)
         if form.is_valid():
-            coach = form.save()
+            form.save()
             return redirect('coaches_list')
     else:
         form = CoachModelForm()
--- courses/views.py
+++ courses/views.py
@@ -5,17 +5,12 @@ from courses.models import Course
 from django import forms
 
 
-class CoursesForm(forms.Form):
-    TECHNOLOGY_CHOICE = (('p', 'Python'),
-                         ('r', 'Ruby'),
-                         ('h', "Php"))
-    course_name = forms.CharField(label='Name', max_length=100)
-    course_description = forms.CharField(label='Description', max_length=255)
-#    course_coach = forms.ModelChoiceField(queryset=Coach.objects.all())
-#    course_assistant = forms.ModelChoiceField(queryset=Coach.objects.all())
-    course_startdate = forms.DateTimeField()
-    course_enddate = forms.DateTimeField()
-    course_technology = forms.ChoiceField(choices=TECHNOLOGY_CHOICE, widget=forms.RadioSelect)
+class CoursesModelForm(forms.ModelForm):
+	class Meta:
+		model = Course
+		#fields = ['first_name', 'last_name', 'phone_number', 'courses', 'package']
+		exclude = ['slug', 'venue_adv']
+
 
 def courses_list(request):
     courses = Course.objects.all()
@@ -26,60 +21,32 @@ def course_info(request, course_id):
     course = get_object_or_404(Course, id=course_id)
     return render(request, 'courses/courses_details.html', {'course': course})
 
+
 def course_edit(request, course_id):
+    title = "Course edit item"
     course = Course.objects.get(id=course_id)
     if request.method == 'POST':
-        form = CoursesForm(request.POST)
+        form = CoursesModelForm(request.POST, instance=course)
         if form.is_valid():
-            course.name = form.cleaned_data['course_name']
-            course.description = form.cleaned_data['course_description']
- #           course.coach = form.cleaned_data['course_coach']
- #           course.assistant = form.cleaned_data['course_assistant']
-            course.start_date = form.cleaned_data['course_startdate']
-            course.end_date = form.cleaned_data['course_enddate']
-            course.technology = form.cleaned_data['course_technology']
-            course.save()
-            # process
-            return redirect('course_edit', course_id)
+            course = form.save()
+            return redirect('courses_list')
     else:
-        form = CoursesForm(initial={'course_name':course.name,
-        	                        'course_description':course.description,
-                                    #'course_coach':course.coach,
-                                    #'course_assistant':course.assistant,
-                                    'course_startdate':course.start_date,
-                                    'course_enddate':course.end_date,
-                                    'course_technology':course.technology,
-                                    })
-    return render(request, 'courses/courses_edit.html', {'form': form})
-
-#def student_edit(request, student_id):
-#    title = "Student edit item"
-#    if student_id is None:
-#        student = Student()
-#    else:
-#        student = Student.objects.get(id=student_id)
-#    if request.method == 'POST':
-#        form = StudentModelForm(request.POST, instance=student)
-#        if form.is_valid():
-#            student = form.save()
-#            return redirect('students_list')
-#    else:
-#        form = StudentModelForm(instance=student)
-#    return render(request, 'students/student_edit.html', {'form': form, 'title': title})
+        form = CoursesModelForm(instance=course)
+    return render(request, 'courses/courses_edit.html', {'form': form, 'title': title})
 
 
 def course_add(request):
     title = "Course add item"
     if request.method == 'POST':
-        form = CoursesForm(request.POST)
+        form = CoursesModelForm(request.POST)
         if form.is_valid():
             course = form.save()
             return redirect('courses_list')
     else:
-        form = CoursesForm()
-    return render(request, 'students/student_edit.html', {'form': form, 'title': title})
+        form = CoursesModelForm()
+    return render(request, 'courses/courses_edit.html', {'form': form, 'title': title})
 
 def course_delete(request, course_id):
-    course = get_object_or_404(Courses, id=course_id)
+    course = get_object_or_404(Course, id=course_id)
     course.delete()
     return redirect('courses_list')
